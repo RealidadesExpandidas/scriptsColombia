c356cbd9e320affa62e0b79280f1abf981293988
baronrampante <baronrampante@users.noreply.github.com>
2022-09-29 12:24:07 -0300

    Update DistanciaAvatar2.cs

--- DistanciaAvatar2.cs
+++ DistanciaAvatar2.cs
@@ -15,33 +15,56 @@ public class DistanciaAvatar2 : MonoBehaviour
     public Transform pos3;
     float distancia;
     int cambio = 0;
+    int posicion = 0;
     // Start is called before the first frame update
     void Start()
     {
-        distancia = Vector3.Distance(fruidor.GetComponent<Transform>().position, avatar.GetComponent<Transform>().position);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (distancia < distanciaAvatar)
+
+        distancia = Vector3.Distance(fruidor.GetComponent<Transform>().position, avatar.GetComponent<Transform>().position);
+       
+        print("distancia" + distancia + "   " + cambio);
+
+        // primer ciclo deteccion y movimiento
+
+        if (distancia < distanciaAvatar && cambio == 0) // primera deteccion y movimiento
         {
+                       cambio = 1;
+        }
 
-            switch (cambio){
+        if (cambio == 1)// se mueve hasta la primera posición
+        {
+            avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, pos1.transform.position, velocidad * Time.deltaTime);
+        }
 
-                case 0:
-                    avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, pos1.transform.position, velocidad * Time.deltaTime);
-                    cambio = cambio + 1;
-                    break;
-                case 1:
-                    avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, pos2.transform.position, velocidad * Time.deltaTime);
-                    cambio = cambio + 1;
-                    break;
-                case 2:
-                    avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, pos3.transform.position, velocidad * Time.deltaTime);
-                    cambio = cambio + 1;
-                    break;
-            }
+
+        if (avatar.transform.position == pos1.transform.position)//si llega a la primera posición 
+        {
+            cambio = 2;
+        }
+
+        // segundo ciclo deteccion y movimiento
+
+        if (distancia < distanciaAvatar && cambio == 2) // segunda  deteccion y movimiento
+        {
+            cambio = 3;
+        }
+
+        if (cambio == 3)// se mueve a la segunda posicion
+        {
+            avatar.transform.position = Vector3.MoveTowards(avatar.transform.position, pos2.transform.position, velocidad * Time.deltaTime);
+        }
+        if (avatar.transform.position == pos2.transform.position)//si llega a la segunda posición 
+        {
+            cambio = 4;
         }
     }
+
+   
 }
+
